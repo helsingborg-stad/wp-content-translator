20c8ab8e11169540547bfac25ca515e0fcd00d17
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-01-12 13:10:34 +0100

    Commented code examples.

--- source/php/App.php
+++ source/php/App.php
@@ -9,6 +9,23 @@ class App
         add_action('admin_enqueue_scripts', array($this, 'adminEnqueue'));
 
         new Admin\Options();
+
+
+        add_action('init', function() {
+            global $wpdb;
+
+
+            //Avabile
+            //var $ms_global_tables = array( 'blogs', 'signups', 'site', 'sitemeta', 'sitecategories', 'registration_log', 'blog_versions' );
+            //var $ms_global_tables = array( 'blogs', 'signups', 'site', 'sitemeta','sitecategories', 'registration_log', 'blog_versions' );
+
+
+            //Filter: query add_filter('query', function($sql) {});
+
+            var_dump($wpdb->tables);
+            $wpdb->tables[0] = "test";
+            var_dump($wpdb->tables);
+        });
     }
 
     public function adminEnqueue()
