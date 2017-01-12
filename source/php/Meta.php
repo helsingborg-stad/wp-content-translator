d4268280dddb19f82f0b50984f9c001485c02519
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-01-12 19:55:32 +0100

    Use plugin language and prepare for save/update action

--- source/php/Meta.php
+++ source/php/Meta.php
@@ -5,21 +5,26 @@ namespace ContentTranslator;
 class Meta Extends Entity\Translate
 {
 
-    protected $lang;
-
     public function __construct()
     {
 
-        $this->lang = "en";
-
+        // Get actions
         add_filter('get_post_metadata',array($this,'get'),4,1);
+
+        // Add and update actions
+        foreach (array('add','update') as $action) {
+            add_filter($action . '_post_metadata', array($this, 'save'));
+        }
+
     }
 
-    public function save() {
+    public function save(null, $object_id, $meta_key, $single ) {
 
+        return null;
     }
 
-    public function get(null, $object_id, $meta_key, $single ) {
+    public function get(null, $object_id, $meta_key, $single )
+    {
         if(!$this->isLangualMeta($meta_key)) {
             return get_metadata('post', $object_id, $this->createLangualMetaKey($meta_key), $single);
         }
@@ -27,11 +32,11 @@ class Meta Extends Entity\Translate
     }
 
     private function isLangualMeta($meta_key) {
-        return substr($meta_key, -strlen("_".$this->lang)) == "_".$this->lang ? true : false;
+        return substr($meta_key, -strlen("_".\ContentTranslator\Switcher::$currentLanguage->code) == "_".\ContentTranslator\Switcher::$currentLanguage->code ? true : false;
     }
 
     private function createLangualMetaKey ($meta_key) {
-        return $meta_key."_".$this->lang;
+        return $meta_key."_".\ContentTranslator\Switcher::$currentLanguage->code;
     }
 
 }
