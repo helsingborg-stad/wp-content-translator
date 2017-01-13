14b0815e69547f4dea0d294d70df99d11f3c5174
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-01-13 11:02:35 +0100

    Do not translate hidden meta or keys specified in settings array.
    Added the ability to filter default return.

--- source/php/Meta.php
+++ source/php/Meta.php
@@ -15,14 +15,16 @@ class Meta Extends Entity\Translate
         }
     }
 
-    public function save(null, $object_id, $meta_key, $single ) {
+    public function save(null, $object_id, $meta_key, $single )
+    {
 
         return null;
     }
 
-    public function get(null, $object_id, $meta_key, $single ) {
+    public function get(null, $object_id, $meta_key, $single )
+    {
 
-        if(!$this->isLangualMeta($meta_key)) {
+        if(!$this->isLangualMeta($meta_key) && $this->shouldTranslate($meta_key)) {
 
             $translation =  get_metadata('post', $object_id, $this->createLangualMetaKey($meta_key), $single);
 
@@ -33,17 +35,32 @@ class Meta Extends Entity\Translate
             } {
                 return $translation; // Not empty, return value
             }
-
         }
 
         return null;
     }
 
-    private function isLangualMeta($meta_key) {
+    private function shouldTranslate($meta_key)
+    {
+        if(!TRANSLATE_HIDDEN_META && substr($meta_key, 1) == "_") {
+            return false;
+        }
+
+        if(in_array($meta_key, UNTRANSLATEBLE_META)) {
+            return false;
+        }
+
+        return apply_filters('wp-content-translator/meta/should_translate_default', true, $meta_key);
+
+    }
+
+    private function isLangualMeta($meta_key)
+    {
         return substr($meta_key, -strlen(TRANSLATE_DELIMITER.$this->lang)) == TRANSLATE_DELIMITER.$this->lang ? true : false;
     }
 
-    private function createLangualMetaKey ($meta_key) {
+    private function createLangualMetaKey ($meta_key)
+    {
         return $meta_key.TRANSLATE_DELIMITER.$this->lang;
     }
 }
