d188f0caea5da6f1e2d9f68a662910c463c3e3c0
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-13 08:59:26 +0100

    is language set public function

--- Public.php
+++ Public.php
@@ -1,3 +1,8 @@
 <?php
 
-// Public functions
+if (!function_exists('wp_content_translator_is_language_set')) {
+    function wp_content_translator_is_language_set()
+    {
+        return \ContentTranslator\Switcher::isLanguageSet();
+    }
+}
--- source/php/Switcher.php
+++ source/php/Switcher.php
@@ -21,6 +21,11 @@ class Switcher
         self::$currentLanguage = \ContentTranslator\Language::find($code);
     }
 
+    /**
+     * Checks if language is set
+     * Also available as public function: wp_content_translator_is_language_set()
+     * @return boolean
+     */
     public static function isLanguageSet()
     {
         return isset(self::$currentLanguage) && !is_null(self::$currentLanguage) && !empty(self::$currentLanguage);
