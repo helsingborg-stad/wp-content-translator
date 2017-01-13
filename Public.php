4641e2f4e61a4faba97ed379a0585e40dea84c68
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-13 09:02:43 +0100

    Public function to get current language

--- Public.php
+++ Public.php
@@ -1,8 +1,15 @@
 <?php
 
 if (!function_exists('wp_content_translator_is_language_set')) {
-    function wp_content_translator_is_language_set()
+    function wp_content_translator_is_language_set() : bool
     {
         return \ContentTranslator\Switcher::isLanguageSet();
+    }
+}
+
+if (!function_exists('wp_content_translator_current_language')) {
+    function wp_content_translator_current_language() : \stdClass
+    {
+        return \ContentTranslator\Switcher::$currentLanguage;
     }
 }
