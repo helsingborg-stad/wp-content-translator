998f7b0f0771359aa26a815b9c16b76742eb0c46
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-17 11:35:06 +0100

    Make createLangualKey and isLangualKey private

--- source/php/Entity/Translate.php
+++ source/php/Entity/Translate.php
@@ -19,7 +19,7 @@ abstract class Translate
      * @param  string $key The meta/option key
      * @return string      Langual meta/option key
      */
-    protected function createLangualKey(string $key) : string
+    private function createLangualKey(string $key) : string
     {
         if ($this->isLangualOption($key)) {
             return $key;
@@ -33,7 +33,7 @@ abstract class Translate
      * @param  string  $key Option key
      * @return boolean
      */
-    protected function isLangualOption($key)
+    private function isLangualOption($key)
     {
         return substr($key, -strlen(TRANSLATE_DELIMITER . $this->lang)) == TRANSLATE_DELIMITER . $this->lang ? true : false;
     }
