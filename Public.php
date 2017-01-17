a2766a616310538c44a81b0e555ee67d89f5e551
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-17 11:10:42 +0100

    Add documentation to public functions

--- Public.php
+++ Public.php
@@ -1,6 +1,10 @@
 <?php
 
 if (!function_exists('wp_content_translator_is_language_set')) {
+    /**
+     * Check if language is set
+     * @return bool
+     */
     function wp_content_translator_is_language_set() : bool
     {
         return \ContentTranslator\Switcher::isLanguageSet();
@@ -8,6 +12,10 @@ if (!function_exists('wp_content_translator_is_language_set')) {
 }
 
 if (!function_exists('wp_content_translator_current_language')) {
+    /**
+     * Gets the current language and it's attributes
+     * @return \stdClass
+     */
     function wp_content_translator_current_language() : \stdClass
     {
         return \ContentTranslator\Switcher::$currentLanguage;
@@ -15,7 +23,13 @@ if (!function_exists('wp_content_translator_current_language')) {
 }
 
 if (!function_exists('wp_content_translator_languages')) {
-    function wp_content_translator_languages(string $level = 'active')
+    /**
+     * Get a list of all languages matching the "level"
+     * Available levels: all, active, installed, uninstalled
+     * @param  string $level Level
+     * @return array
+     */
+    function wp_content_translator_languages(string $level = 'active') : array
     {
         if (!method_exists('\ContentTranslator\Language', $level)) {
             $level = 'active';
@@ -53,22 +67,35 @@ if (!function_exists('wp_content_translator_languages')) {
 }
 
 if (!function_exists('wp_content_translator_unparse_url')) {
-    function wp_content_translator_unparse_url(array $parsed_url)
+    /**
+     * Unparses url (parse_url() function)
+     * @param  array  $string The array of the parse_url() function
+     * @return string
+     */
+    function wp_content_translator_unparse_url(array $parsedUrl) : string
     {
-        $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-        $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-        $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-        $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
+        $host     = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
+        $port     = isset($parsedUrl['port']) ? ':' . $parsedUrl['port'] : '';
+        $user     = isset($parsedUrl['user']) ? $parsedUrl['user'] : '';
+        $pass     = isset($parsedUrl['pass']) ? ':' . $parsedUrl['pass']  : '';
         $pass     = ($user || $pass) ? "$pass@" : '';
-        $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-        $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+        $path     = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
+        $query    = isset($parsedUrl['query']) ? '?' . $parsedUrl['query'] : '';
+        $fragment = isset($parsedUrl['fragment']) ? '#' . $parsedUrl['fragment'] : '';
 
         return "//$user$pass$host$port$path$query$fragment";
     }
 }
 
 if (!function_exists('wp_content_translator_language_selector')) {
+    /**
+     * Renders a language selector
+     * @param  string|null  $wrapper   Wrapper template
+     * @param  string|null  $element   Element template
+     * @param  bool|boolean $echo      Echo or not
+     * @param  array|null   $languages Languages to use in selector
+     * @return string                  HTML Markup of the selector
+     */
     function wp_content_translator_language_selector(string $wrapper = null, string $element = null, bool $echo = true, array $languages = null)
     {
         return \ContentTranslator\Switcher::selector($wrapper, $element, $echo, $languages);
