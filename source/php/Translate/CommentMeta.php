10e1adea02059dfdddd9b1abe6defa24780d1cd2
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-01-18 14:11:52 +0100

    Construct parents in user & comment.

--- source/php/Translate/CommentMeta.php
+++ source/php/Translate/CommentMeta.php
@@ -6,6 +6,8 @@ class CommentMeta extends Meta
 {
     public function __construct()
     {
+        parent::__construct();
+
         if ($this->configuration->comment->translate) {
             parent::__construct('comment');
         }
